69b2bdd18bfaae4d9580a7ac9c16e56d15f56c87
Alvin Loh <alvinloh@tuaspower.com.sg>
2019-08-29 17:51:42 +0800

    First commit

--- helpers/auth.js
+++ helpers/auth.js
@@ -1,4 +1,17 @@
+require('dotenv').config();
 
 // Create a new auth object
 var auth = {};
 
+// Populate our keys
+auth.twitter_oauth = {
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    token: process.env.TWITTER_ACCESS_TOKEN,
+    token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+}
+
+auth.twitter_webhook_environment = process.env.TWITTER_WEBHOOK_ENV
+auth.twitter_webhook_url = process.env.TWITTER_WEBHOOK_URL
+
+module.exports = auth;
--- routes/webhook.js
+++ routes/webhook.js
@@ -1,11 +1,56 @@
 var express = require('express');
+var auth = require('../helpers/auth');
+var axios = require('axios');
 var router = express.Router();
 
 
+// Helper function to get the bearer token
+const getBearerToken = async function(){
+    let bearer_token = await  axios({
+        method: 'post',
+        //url: 'https://ptsv2.com/t/sj1fv-1567066554/post',
+        url: 'https://api.twitter.com/oauth2/token',
+        auth: {username: auth.twitter_oauth.consumer_key, password: auth.twitter_oauth.consumer_secret},
+        headers: {'Content-Type': 'x-www-form-urlencoded'},
+        params: {grant_type: 'client_credentials'}
+    });
 
-// List out the environment webhooks available
-router.get('/', function(req, res, next) {
-    res.send('inside webhook router');
+    return bearer_token.data;
+};
+
+// Helper function to get the active webhook
+const getWebHook = async function(){
+    let bearer_token_response = await getBearerToken();
+    let bearer_token = bearer_token_response.access_token;
+
+    let webhookinfo = await axios({
+        method: 'get',
+        url: 'https://api.twitter.com/1.1/account_activity/all/' + auth.twitter_webhook_environment + '/webhooks.json',
+        headers: {'authorization': 'Bearer ' + bearer_token}
+    })
+
+    return webhookinfo.data;
+};
+
+// Helper function to delete the webhook
+const deleteWebHook = async function(webhookid){
+    axios({
+        method: 'delete',
+        url: 'https://api.twitter.com/1.1/account_activity/all/' + auth.twitter_webhook_environment + '/webhooks/' + webhookid + '.json',
+        headers{'authorization': 'OAuth' + }
+    })
+}
+
+// Get all registered webhooks
+router.get('/list', async function (req, res) {
+    let webhookinfo = await getWebHook();
+    res.send(webhookinfo);
+
 });
+
+
+
+
+
 
 module.exports = router;
